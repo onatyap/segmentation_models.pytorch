f705d2f3bb0eaeca7152067fa7cdc29355e30404
Onat Yapici <oyapici17@ku.edu.tr>
2021-06-01 10:09:13 +0300

    Fix typos
    Fixed the typos in documentation

--- segmentation_models_pytorch/losses/constants.py
+++ segmentation_models_pytorch/losses/constants.py
@@ -1,12 +1,12 @@
 #: Loss binary mode suppose you are solving binary segmentation task.
-#: That mean yor have only one class which pixels are labled as **1**,
+#: That mean you have only one class which pixels are labeled as **1**,
 #: the rest pixels are background and labeled as **0**.
 #: Target mask shape - (N, H, W), model output mask shape (N, 1, H, W).
 BINARY_MODE: str = "binary"
 
 #: Loss multiclass mode suppose you are solving multi-**class** segmentation task.
 #: That mean you have *C = 1..N* classes which have unique label values,
-#: classes are mutually exclusive and all pixels are labeled with theese values.
+#: classes are mutually exclusive and all pixels are labeled with these values.
 #: Target mask shape - (N, H, W), model output mask shape (N, C, H, W).  
 MULTICLASS_MODE: str = "multiclass"
 
